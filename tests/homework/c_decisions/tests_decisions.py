84b738f69ab1048429482c86d5eb62eae8657a43
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-02-12 23:03:59 +0000

    Test Fix for Decisions.py

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -3,6 +3,7 @@ from src.homework.c_decisions.decisions import get_options_ratio
 from src.homework.c_decisions.decisions import get_faculty_rating
 
 class Test_Config(unittest.TestCase):
-    def test_get_options_ratio():
-        return get_options_ratio(5,20)
-    
+    def test_get_options_ratio(self):
+        self.assertEqual(get_options_ratio(5,20),0.25)
+    def test_get_faculty_rating(self):
+        self.assertEqual(get_faculty_rating(0.9),"Excellent")
