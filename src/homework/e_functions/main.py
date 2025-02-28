1eef8747f280f4b9c0afac5d83857ba4e7aa0d76
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-02-28 01:02:27 +0000

    Homework 4 Submission

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,18 @@
-#
+from value_return import get_hours
+from value_return import get_minutes
+from value_return import get_seconds
+
+def display_human_clock(epoch_sec):
+    hours= get_hours(epoch_sec)
+    mins= get_minutes(epoch_sec)
+    secs= get_seconds(epoch_sec)
+    print(str(time_to_txt(hours))+":"+str(time_to_txt(mins))+":"+str(time_to_txt(secs)))
+
+def time_to_txt(mhs):
+    timetxt= ""
+    if mhs < 10:
+        timetxt= "0"+str(mhs)
+    else:
+        timetxt= str(mhs)
+    return timetxt
+
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,11 @@
-#
+def get_hours(epoch_sec):
+    hours= int((epoch_sec/3600)%24)
+    return hours
+
+def get_minutes(epoch_sec):
+    mins= int((epoch_sec/60)%60)
+    return mins
+
+def get_seconds(epoch_sec):
+    secs= int(epoch_sec%60)
+    return secs
