6f80180d3f811a3632f5cf6935451c16625d3a24
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-03-06 04:01:37 +0000

    HW5 Submission Commit

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,31 @@
-#
+from strings import get_hamming_distance
+from strings import get_dna_complement
+
+def hw5_menu():
+    choice = 0
+    while choice ==0:
+        choice = int(input("Homework 5 Menu\n1-Hamming Distance\n2-Dna Complement\n3-Exit\n"))
+        while choice == 1:
+            dna1 = str(input("Enter First Dna Sequence\n"))
+            dna2 = str(input("Enter Second Dna Sequence\n"))
+            ans = get_hamming_distance(dna1,dna2)
+            print("Answer: " + str(ans))
+            exvar = input("Do you want to exit?\ny or n\n")
+            if exvar == "y":
+                choice = 0
+            else:
+                choice = 1
+        while choice == 2:
+            dna = str(input("Enter a Dna Sequence\n"))
+            ans = get_dna_complement(dna)
+            print("Answer: " + str(ans))
+            exvar = input("Do you want to exit?\ny or n\n")
+            if exvar == "y":
+                choice = 0
+            else:
+                choice = 2
+        if choice == 3:
+            print("Exiting Program...")
+            exit
+
+hw5_menu()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,24 @@
-#
+def get_hamming_distance(dna1,dna2):
+    hamming= 0
+    for char in range(len(dna1)):
+        if dna1[char]!=dna2[char]:
+            hamming+=1
+    return hamming
+            
+def get_dna_complement(dna):
+    rev= ''
+    revcomp= ''
+    for char in range(len(dna)):
+        rev+=str(dna[-char-1])
+    print(rev)
+    for char in range(len(rev)):
+        if str(rev[char]) in 'Aa':
+            revcomp+= 'T'
+        if str(rev[char]) in 'Tt':
+            revcomp+= 'A'
+        if str(rev[char]) in 'Cc':
+            revcomp+= 'G'
+        if str(rev[char]) in 'Gg':
+            revcomp+= 'C'
+    return revcomp
+
