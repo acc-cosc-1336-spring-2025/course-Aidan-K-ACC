f97205ec520ee5da4811108435a63210cf6e74bd
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-03 14:19:51 +0000

    main creation

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,23 +1,16 @@
-from lists import get_highest_list_value
-from lists import get_lowest_list_value
+from lists import get_p_distance
+from lists import get_p_distance_matrix
 
 def hw6_menu():
     choice = 0
     while choice ==0:
-        choice = int(input("Homework 6 Menu\n1 Show the list low/high values-\n2-Exit\n"))
+        choice = int(input("Homework 6 Menu\n1 Get p distance matrix-\n2-Exit\n"))
         while choice == 1:
-            list= []
-            while len(list) < 3:
-                list+= input("Enter a number.\n")
-            choice2= input("Do you want to enter another value? y or n\n")
-            if choice2 == 'y':
-                while choice2 == 'y':
-                    kbin= input("Enter a number, or press 'n' \n")
-                    if kbin != 'n':
-                        list+= kbin
-                    if kbin == 'n':
-                        choice2= 'n'
-            ans = "Highest: "+get_highest_list_value(list)+"   "+"Lowest: "+get_lowest_list_value(list)
+            data_matrix= []
+            temp_list= []
+            num= int(input("How many sequences are in the data set?\n"))
+            for 
+            ans = "P distance test results: " + get
             print("Answer: " + str(ans))
             exvar = input("Do you want to Exit?\ny or n\n")
             if exvar == "y":
