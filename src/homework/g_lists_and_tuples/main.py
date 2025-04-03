db14307bc31238cdefa72621edfbffa00fac5f9e
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-03 18:09:21 +0000

    Homework 7 Submission

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,17 +1,24 @@
 from lists import get_p_distance
 from lists import get_p_distance_matrix
 
-def hw6_menu():
+def hw7_menu():
     choice = 0
     while choice ==0:
         choice = int(input("Homework 6 Menu\n1 Get p distance matrix-\n2-Exit\n"))
         while choice == 1:
             data_matrix= []
-            temp_list= []
             num= int(input("How many sequences are in the data set?\n"))
-            for 
-            ans = "P distance test results: " + get
-            print("Answer: " + str(ans))
+            for n in range(0, num):
+                temp_list= []
+                key= ''
+                print("Enter the data for sequence " + str(n+1) + " and type end when complete.")
+                while key != 'end':
+                    key= input("")
+                    if key != 'end':
+                        temp_list.append(key)
+                data_matrix.append(temp_list)
+            ans = get_p_distance_matrix(data_matrix)
+            print("P distance results: " + str(ans))
             exvar = input("Do you want to Exit?\ny or n\n")
             if exvar == "y":
                 choice = 0
@@ -21,4 +28,4 @@ def hw6_menu():
             print("Exiting Program...")
             exit
 
-hw6_menu()
+hw7_menu()
