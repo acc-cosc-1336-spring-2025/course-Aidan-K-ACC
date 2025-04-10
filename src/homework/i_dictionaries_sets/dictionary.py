a3ca7cc59e75f5d300e95978469913e78c24d66c
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-10 00:51:32 +0000

    HW8 in progress

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,23 @@
-#
+
+def add_inventory(dictionary, widget_name, quantity):
+    if widget_name in dictionary:
+        dictionary[widget_name]= dictionary[widget_name] + quantity
+    if widget_name not in dictionary:
+        dictionary[widget_name]= quantity
+
+def remove_inventory_widget(dictionary, widget_name):
+    if widget_name in dictionary:
+        dictionary.pop(widget_name)
+        return("Record deleted")
+    else:
+        return "Item not found"
+
+
+dictionary= {}
+
+add_inventory(dictionary, 'Widget1', 10)
+add_inventory(dictionary, 'Widget1', 25)
+print(dictionary)
+remove_inventory_widget(dictionary, 'Widget1')
+print(remove_inventory_widget(dictionary, 'Widget1'))
+print(dictionary)
