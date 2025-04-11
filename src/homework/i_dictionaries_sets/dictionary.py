04d40a57e168c002374a22ecc8f64c98f98e9b3f
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-11 01:20:42 +0000

    Homework 8 Test Functs Done

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -8,16 +8,6 @@ def add_inventory(dictionary, widget_name, quantity):
 def remove_inventory_widget(dictionary, widget_name):
     if widget_name in dictionary:
         dictionary.pop(widget_name)
-        return("Record deleted")
+        return "Record deleted"
     else:
         return "Item not found"
-
-
-dictionary= {}
-
-add_inventory(dictionary, 'Widget1', 10)
-add_inventory(dictionary, 'Widget1', 25)
-print(dictionary)
-remove_inventory_widget(dictionary, 'Widget1')
-print(remove_inventory_widget(dictionary, 'Widget1'))
-print(dictionary)
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,21 @@
-#
+import unittest
+
+from src.homework.i_dictionaries_sets.dictionary import add_inventory
+from src.homework.i_dictionaries_sets.dictionary import remove_inventory_widget
+
+class Test_Config(unittest.TestCase):
+    def test_add_inventory(self):
+        dictionary= {}
+        add_inventory(dictionary, 'Widget1', 10)
+        self.assertEquals(dictionary, {'Widget1':10})
+        add_inventory(dictionary, 'Widget1', 25)
+        self.assertEquals(dictionary, {'Widget1':35})
+        add_inventory(dictionary, 'Widget1', -10)
+        self.assertEquals(dictionary, {'Widget1':25})
+    
+    def test_remove_inventory_widget(self):
+        dictionary= {'Widget1':1,'Widget2':2}
+        remove_inventory_widget(dictionary, 'Widget1')
+        self.assertEquals(len(dictionary)==1, True)
+        self.assertEquals('Widget2' in dictionary.keys(), True)
+    
