6ed225c3790053d182d0892eeba3fcc07ee1d913
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-24 00:57:31 +0000

    classes test cases complete

--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,13 @@
-#
+import unittest
+
+from src.homework.j_classes.class_a import die
+
+class Test_Config(unittest.TestCase):
+    def test_die(self):
+        die.roll()
+        self.assertEqual(die.roll_value>=1 and die.roll_value<=6, True)
+        die.roll()
+        self.assertEqual(die.roll_value>=1 and die.roll_value<=6, True)
+        die.roll()
+        self.assertEqual(die.roll_value>=1 and die.roll_value<=6, True)
+
