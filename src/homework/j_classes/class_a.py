fcfc9f684d8f729e9ca1820f6f87c74e0f730035
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-24 00:44:40 +0000

    Die Class complete

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -2,9 +2,12 @@
 import random
 
 class die:
+    roll_value= 0
     def roll():
-        return random.randint(1,6)
-    def get_roll():
-        return die.roll()
-    
-print(die.get_roll())
+        die.roll_value= random.randint(1,6)
+        return die.roll_value
+    def get_rolled_value():
+        if die.roll_value==0:
+            die.roll_value= die.roll()
+        return "The rolled value is "+ str(die.roll_value)
+
