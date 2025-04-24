3abc9ac967aefbf0c71d72a287aadee67ee38a8a
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-04-24 01:06:35 +0000

    Homework 9 Submit

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,12 @@
-#
+from class_a import die
+
+def Homework_9_Menu():
+    userin_yn= input("Would you like to roll the dice? y/n\n")
+    while userin_yn == 'y':
+        die.roll()
+        print(die.get_rolled_value())
+        userin_yn= input("Would you like to roll again? y/n\n")
+    if userin_yn == 'n':
+        print("Exiting...")
+
+Homework_9_Menu()
